8d5a7ceea7d1557543b4d9c0a584dd05b2c4995a
Ankith Konda <a.konda@uq.edu.au>
2016-03-09 16:34:38 +1000

    Add Asynchronous image to base64 conversion

--- index.php
+++ index.php
@@ -10,39 +10,6 @@
 <!-- Answer a bunch of questions and a pdf will be generated -->
 
 
-<script type="text/javascript">
-
-$(document).ready(function() {
-
-	console.log('hello world');
-
-
-	var documentdef = {
-		content: [
-			'First paragraph',
-			'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-		]
-	}
-
-	$(".makepdf").click(function(event) {
-
-		 pdfMake.createPdf(documentdef).download('awesome.pdf');
-
-	})
-
-});
-
-</script>
-
-<script type="text/javascript" src="www/js/pdfmake/pdfmake.min.js"></script>
-<script type="text/javascript" src="www/js/pdfmake/vfs_fonts.js"></script>
-
-
-
-
-
-
-
 <?php
 
 
@@ -72,88 +39,88 @@ $(document).ready(function() {
 
 
 <div id="qustion_1" class="question_container">
-	<h3>Extroversion or introversion</h3>
+	<span class="titles">Extroversion or introversion</span>
 	<p>Would you consider yourself an extrovert or introvert?</p>
 	<div class="input_container extro_intro_inputs">
-		<input type="radio" name="extro_intro_input" id="introvert_input"	value="Introvert">Introvert</br>
-		<input type="radio" name="extro_intro_input" id="equal_input"	value="Equal traits of both">Equal traits of both</br>
-		<input type="radio" name="extro_intro_input" id="extrovert_input"	value="Extrovert">Extrovert</br>
+		<input class="target" type="radio" name="extro_intro_input" id="introvert_input"	value="Introvert">Introvert</br>
+		<input class="target" type="radio" name="extro_intro_input" id="equal_input"	value="Equal">Equal traits of both</br>
+		<input class="target" type="radio" name="extro_intro_input" id="extrovert_input"	value="Extrovert">Extrovert</br>
 	</div>
 </div>
 
 <div id="qustion_2" class="question_container">
-	<h3>Friendliness</h3>
+	<span class="titles">Friendliness</span>
 	<p>select the option that best describes your level of friendliness</p>
 
 	<div class="input_container friendliness_inputs">
-		<input type="radio" name="friendliness_input" id="low_input"	value="Low">Low</br>
-		<input type="radio" name="friendliness_input" id="medium_input"	value="Medium">Medium</br>
-		<input type="radio" name="friendliness_input" id="high_input"	value="High">High</br>
+		<input class="target" type="radio" name="friendliness_input" id="low_input"	value="Low">Low</br>
+		<input class="target" type="radio" name="friendliness_input" id="medium_input"	value="Medium">Medium</br>
+		<input class="target" type="radio" name="friendliness_input" id="high_input"	value="High">High</br>
 	</div>
 </div>
 
 <div id="qustion_3" class="question_container">
-	<h3>Team Interest or Self Interest</h3>
+	<span class="titles">Team Interest or Self Interest</span>
 	<p>On a scale of team versus self-interest where does your motivation lie?</p>
 
 	<div class="input_container interest_inputs">
-		<input type="radio" name="interest_input" id="1_input"	value="1">1 - Team Interest</br>
-		<input type="radio" name="interest_input" id="2_input"	value="2">2</br>
-		<input type="radio" name="interest_input" id="3_input"	value="3">3</br>
-		<input type="radio" name="interest_input" id="4_input"	value="4">4</br>
-		<input type="radio" name="interest_input" id="5_input"	value="5">5 - Self Interest</br>
+		<input class="target" type="radio" name="interest_input" id="1_input"	value="1">1 - Team Interest</br>
+		<input class="target" type="radio" name="interest_input" id="2_input"	value="2">2</br>
+		<input class="target" type="radio" name="interest_input" id="3_input"	value="3">3</br>
+		<input class="target" type="radio" name="interest_input" id="4_input"	value="4">4</br>
+		<input class="target" type="radio" name="interest_input" id="5_input"	value="5">5 - Self Interest</br>
 	</div>
 </div>
 
 
 <div id="question_4" class="question_container">
 
-<h3>Team role preferences</h3>
+<span class="titles">Team role preferences</span>
 	<p>Indicate your preference for the team roles below with:</p>
 
 	<div class="input_container preferences_inputs">
 		<b>First preference:</b>
-		<select class="preference_dropdown">
-				<option value=""></option>
+		<select name="first_preference" class="preference_dropdown target">
+				<option selected disabled hidden style='display: none' value=''></option>
 				<option value="Coordinator">Coordinator</option>
 				<option value="Shaper">Shaper</option>
 				<option value="Implementer">Implementer</option>
 				<option value="Plant">Plant</option>
-				<option value="Team worker">Team worker</option>
-				<option value="Monitor/evaluator">Monitor/evaluator</option>
-				<option value="Completer/finisher">Completer/finisher</option>
-				<option value="Resource Investigator">Resource Investigator</option>
+				<option value="TeamWorker">Team worker</option>
+				<option value="Monitor">Monitor/evaluator</option>
+				<option value="Completer">Completer/finisher</option>
+				<option value="Resource_Investigator">Resource Investigator</option>
 		</select>
 	</div>
 
 
 	<div class="input_container preferences_inputs">
 		<b>Second preference:</b>
-		<select class="preference_dropdown">
-				<option value=""></option>
+		<select name="second_preference" class="preference_dropdown target">
+				<option selected disabled hidden style='display: none' value=''></option>
 				<option value="Coordinator">Coordinator</option>
 				<option value="Shaper">Shaper</option>
 				<option value="Implementer">Implementer</option>
 				<option value="Plant">Plant</option>
-				<option value="Team worker">Team worker</option>
-				<option value="Monitor/evaluator">Monitor/evaluator</option>
-				<option value="Completer/finisher">Completer/finisher</option>
-				<option value="Resource Investigator">Resource Investigator</option>
+				<option value="TeamWorker">Team worker</option>
+				<option value="Monitor">Monitor/evaluator</option>
+				<option value="Completer">Completer/finisher</option>
+				<option value="Resource_Investigator">Resource Investigator</option>
 		</select>
 	</div>
 
 	<div class="input_container preferences_inputs">
 		<b>Least prefered role:</b>
-		<select class="preference_dropdown">
-				<option value=""></option>
+		<select name="least_preference" class="preference_dropdown target">
+				<option selected disabled hidden style='display: none' value=''></option>
 				<option value="Coordinator">Coordinator</option>
 				<option value="Shaper">Shaper</option>
 				<option value="Implementer">Implementer</option>
 				<option value="Plant">Plant</option>
-				<option value="Team worker">Team worker</option>
-				<option value="Monitor/evaluator">Monitor/evaluator</option>
-				<option value="Completer/finisher">Completer/finisher</option>
-				<option value="Resource Investigator">Resource Investigator</option>
+				<option value="TeamWorker">Team worker</option>
+				<option value="Monitor">Monitor/evaluator</option>
+				<option value="Completer">Completer/finisher</option>
+				<option value="Resource_Investigator">Resource Investigator</option>
 		</select>
 	</div>
 
@@ -162,8 +129,9 @@ $(document).ready(function() {
 
 </div>
 
-<button class="btn btn-primary btn-md makepdf">Generate PDF</button>     <button id='resetButton' type='reset' class="btn btn-default btn-md">Reset</button>
 
+<button class="btn btn-primary btn-md makepdf">Generate PDF</button>     <button type='reset' class="btn btn-default btn-md resetButton">Reset</button>
+<span class="hint_text">Please complete the survey to generate a PDF</span>
 
 </form>
 
@@ -194,231 +162,229 @@ $(document).ready(function() {
 
 	}
 
-</style>
+	.titles{
+		font-weight:bold;
+		font-size: 20px;
 
+	}
+
+	.hint_text{
+		color: #FFFFFF;
+		display: block;
+		padding:5px;
+		margin-top: 5px;
+		background-color: #FF6128;
+		text-align: center;
+	}
+
+</style>
+<script type="text/javascript" src="www/js/pdfmake/pdfmake.min.js"></script>
+<script type="text/javascript" src="www/js/pdfmake/vfs_fonts.js"></script>
 
 <script type='text/javascript'>
 
+var current_count = 0;
 
+$(document).ready(function() {
 
 
-// 	$(document).ready(function(){
 
-// 		resize();
 
-// 		var fullwidth, pagewidth;
+	toggleGenerateButton('on');
 
 
-// 		$('#feedbackButton').hide();
-// 		$('.score_text').hide();
-// 		var showpage_feature = true;
 
-// 		if(current_response_status == 'finished'){
 
+	console.log('hello world');
 
-// 			var current_score = calculateScore();
 
-// // 			constructScoreFeedback(current_score);
-// 			showpage_feature = false;
-// 			$('#feedbackButton').show();
 
+	// pdfMake.createPdf(documentdef).download('awesome.pdf');
 
+	$(".makepdf").click(function(event) {
 
+		var form_data = $('#survey_form').serializeArray();
+		defineDocument(form_data);
+	});
 
-// 		}
+	$(".resetButton").click(function(event){
+		toggleGenerateButton('off');
+	});
 
-// 		$( window ).resize(function() {
-// 			resize();
-// 		});
-// 		function resize() {
-// 			fullwidth = $('body').width();
-// 			$('div.page_container').width(fullwidth);
-// 			pagewidth = fullwidth;
-// 			$('div.question_page').width(pagewidth-60);
-// 		}
 
+	$('.target').change(function(event){
 
+		var form_data = $('#survey_form').serializeArray();
 
-// 		var currentPage = 1;
-// 		var total_pages = <?php echo count($questions); ?>;
+		if($(form_data).size() == 6){
+			toggleGenerateButton('on');
+		}else{
+			toggleGenerateButton('off');
+		}
 
-// 		var opts = {
-// 		    totalPages: total_pages,
-// 		    visiblePages: 5,
-// 		    startPage:currentPage,
-// 		    onPageClick: function (event, page) {
-// 		        console.log('Page change event. Page = ' + page);
-// 		        $('.pagination').data('currentPage', page);
-// 		        showpage(page);
-// 		    }
-// 		};
+	});
 
-// 		$('.questions_pagination').twbsPagination(opts);
 
-// 		$('.question_input').change(function(){
 
-// 			if(showpage_feature){
+	function defineDocument(data){
 
-// 				var pageto = currentPage+1;
+		function convertImagesToBase64(images, callback){
 
-// 			    if(pageto < opts['totalPages']){
-// 			      $('.questions_pagination').twbsPagination('destroy');
-// 			      $('.questions_pagination').twbsPagination($.extend(opts, {
-// 			          startPage: pageto+1
-// 			      }));
-// 			    }
-// 			}
+			callback(images);
 
-// 		});
+			$.each(images, function(ind, obj){
 
-// 		function showpage(page){
+				var img = new Image();
+			    img.crossOrigin = 'Anonymous';
+			    img.onload = function(){
+			        var canvas = document.createElement('CANVAS');
+			        var ctx = canvas.getContext('2d');
+			        var dataURL;
+			        canvas.height = this.height;
+			        canvas.width = this.width;
+			        ctx.drawImage(this, 0, 0);
+			        dataURL = canvas.toDataURL();
+			        images[ind].base_url = dataURL;
+			        canvas = null;
+			    };
+			    img.src = obj.url;
+			});
 
-// 			console.log(page);
-// 			page = page-1;
+			var alldone = false;
 
-// 			leftm = fullwidth*page*-1;
+			var keepchecking = setInterval(function(){
 
-// 			$( ".page_scroller" ).animate({
-// 			    marginLeft: leftm,
-// 			}, 400);
+				$.each(images,function(inde, ob){
+						if(ob.base_url != 'undefined'){
+							alldone = true;
+						}
+				});
 
-// 			currentPage = page;
+			},500);
 
-// 			//$('.currentpage_status').text((currentPage+1)+'/'+total_pages);
+			if(alldone == true){
+				callback(images);
+				clearInterval(keepchecking);
+			}
 
 
-// 		}
+		}
 
+		var imagesToLoad = [
 
-// 		var showprevious = '<?php echo $pre_qresponse_showanswer ?>';
-// 		var currentStatus = 'unfinished';
-// 		var survey_score = null;
+				{
+					"image_name":"header",
+					"url":"www/images/pdf_header_image.jpg",
+					"base_url":"undefined"
+				},{
+					"image_name":"sample",
+					"url":"sample.jpg",
+					"base_url":"undefined"
+				}
 
-// 		$('#submitButton').click(function(event){
+		];
 
-// 			$(this).addClass('disabled');
 
-// 			$(this).prop("disabled", true);
-//  			$(this).empty().append("Submitting <i class='fa fa-spinner fa-pulse'></i>");
 
-// 			var data = check();
-// 			save(data);
 
-// 		});
+		convertImagesToBase64(imagesToLoad, generatePDF);
 
-// 		$('#resetButton').click(function(event){
 
-// 			reset();
+		function generatePDF(images){
 
-// 		});
 
-// 		function check(){
+			var documentStructure = {
 
-// 			var status = {};
-// 			var statusString;
-// 			var qcount = 0;
-// 			var answeredcount = 0;
+				content: [
+					{
+						image:base,
+					}
+				]
 
+			}
 
+		}
 
-// 			$('.question_container').each(function(){
-// 				qcount++;
-// 				$(this).find('.question_input').each(function(ind, obj){
 
-// 					if($(obj).is(':checked')){
-// 						status["question"+$(obj).data('question_number')] = $(obj).attr('data-option_num');
-// 						answeredcount++;
-// 						return false;
-// 					}else{
-// 						status["question"+$(obj).data('question_number')] = null;
-// 					}
 
-// 				});
-// 			});
+		// var baseImage;
 
-// 			var currentScore = calculateScore();
+		// function getBase64(url, callback, outputFormat){
+		//     var img = new Image();
+		//     img.crossOrigin = 'Anonymous';
+		//     img.onload = function(){
+		//         var canvas = document.createElement('CANVAS');
+		//         var ctx = canvas.getContext('2d');
+		//         var dataURL;
+		//         canvas.height = this.height;
+		//         canvas.width = this.width;
+		//         ctx.drawImage(this, 0, 0);
+		//         dataURL = canvas.toDataURL(outputFormat);
+		//         callback(dataURL);
+		//         canvas = null; 
+		//     };
+		//     img.src = 'pdf_header_image.jpg';
 
-// 			status["score"] = currentScore;
+		//     //set interval to see if the number of images that needed to load have come back, if so do a call back with an array of base64 
+		// }
 
-// 			status["questions_answered_count"] = answeredcount;
 
-// 			status["status"] = 'unfinished';
 
-// 			if(qcount == answeredcount){
-// 				status["status"] = 'finished';
-// 				currentStatus = 'finished';
-// 			}else if(answeredcount > 0){
-// 				status["status"] = 'attempted';
-// 				currentStatus = 'attempted';
+		// toDataUrl('pdf_header_image.jpg', getHeader);
 
-// 			}
 
-// 			statusString = JSON.stringify(status);
 
-// 			return statusString;
+		// setInterval(function(){
 
-// 		}
+		// 	console.log(baseImage);
 
-// 		function save(js_data){
+		// },500);
 
 
 
-// 			var data = {'data':{}};
-// 			data['sid'] = '<?php echo $sid ?>';
-// 			data['user_id'] = '<?php echo $lti->user_id(); ?>';
-// 			data['data'] = $('#questionnaire_form').serialize();
-// 			data['js_data'] =  js_data;
+		// var dd = {
+		// 	content: [
+		// 		{
+		// 			image:base,
+		// 		}
+		// 	]
 
-// 			data['lti_id'] = '<?php echo $lti->lti_id(); ?>';
-// 			data['lis_outcome_service_url'] = '<?php echo $lti->grade_url(); ?>';
-// 			data['lis_result_sourcedid'] = '<?php echo $lti->result_sourcedid(); ?>';
+		// }
 
-// 			$.ajax({
-// 			  type: "POST",
-// 			  url: "savedata.php",
-// 			  data: data,
-// 			  success: function(response) {
 
-// 				  console.log(response);
+		//pdfMake.createPdf(dd).download('awesome.pdf');
 
-// 				$("#submitButton").removeClass('disabled');
-// 				$("#submitButton").prop("disabled", false);
-//  				$("#submitButton").empty().append("Submit");
 
+	}
 
 
-// 			  },
-// 			  error: function(error){
-// 				  console.log(error);
-// 			  }
-// 			});
+	/**
+	 * Toggles the generate pdf button to on or off
+	 * @param  {string} state options["on", "off"]
+	 * @return {null}       No return
+	 */
+	function toggleGenerateButton(state){
+		if(state == "on"){
+			if($('.makepdf').is(":disabled")){
+				$('.makepdf').prop("disabled",false);
+				$(".hint_text").hide();
+			}
+		}else{
+			if(!$('.makepdf').is(":disabled")){
+				$('.makepdf').prop("disabled",true);
+				$(".hint_text").show();
+			}
+		}
+	}
 
 
 
+});
 
-// 		}
-
-// 		function reset(){
-
-// 			console.log('reset');
-
-
-
-// 		}
-
-
-
-
-
-// 	});
 
 
 
 </script>
-
-
-
-
 
 </body>
 </html>
